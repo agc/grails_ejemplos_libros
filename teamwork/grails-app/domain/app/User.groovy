2250df94135722151694bf9c361fe2d54ba3d7fd
agc en Supernova <agc@pnte.cfnavarra.es>
2012-02-12 10:02:01 +0100

    SN Añadidos proyectos para varios libros

---
+++ teamwork/grails-app/domain/app/User.groovy
@@ -0,0 +1,7 @@
+package app
+
+class User {
+
+    static constraints = {
+    }
+}
---
+++ teamwork/test/unit/app/UserSpec.groovy
@@ -0,0 +1,11 @@
+package app
+
+import spock.lang.*
+import grails.plugin.spock.*
+
+class UserSpec extends UnitSpec {
+
+    def "feature method"() {
+
+    }
+}
