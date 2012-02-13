fd0cc41b3b58e837095d57a00ef903a5927290e3
agc en Supernova <agc@pnte.cfnavarra.es>
2012-02-13 22:52:58 +0100

    SN añadido BootStrap, vistas de busqueda y codigo del controlador User

--- hubbub/grails-app/controllers/com/grailsinaction/UserController.groovy
+++ hubbub/grails-app/controllers/com/grailsinaction/UserController.groovy
@@ -3,4 +3,15 @@ package com.grailsinaction
 class UserController {
     def scaffold = true
 
+    def search = {
+
+	}
+
+	def results = {
+
+		def users = User.findAllByUserIdLike("%${params.userId}%")
+		return [ users: users, term : params.userId ]
+
+	}
+
 }
