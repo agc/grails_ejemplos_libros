bfa78d2ef4522d4268c5e04e4db12199a6acb7e8
agc en caracol <agc@secesion.localdomain>
2012-02-28 00:11:26 +0100

    SCSN incluido registroacegi

---
+++ registroacegi/grails-app/controllers/com/grailsinaction/ShopController.groovy
@@ -0,0 +1,79 @@
+package com.grailsinaction
+
+
+class OrderDetailsCommand implements Serializable {
+
+    int numShirts
+    int numHats
+
+    boolean isOrderBlank() {
+        numShirts == 0 && numHats == 0
+    }
+
+    static constraints = {
+        numShirts(range: 0..10)
+        numHats(range: 0..10)
+    }
+}
+
+class ShippingCommand implements Serializable {
+
+    String address
+    String state
+    String postcode
+    String country
+    boolean customShipping
+    String shippingType
+    String shippingOptions
+
+}
+
+class PaymentCommand implements Serializable {
+
+    String cardNumber
+    String name
+    String expiry
+
+    static constraints = {
+        cardNumber(creditCard: true)
+        name(blank: false, maxSize: 50)
+        expiry(matches:"[0-9]{2}/[0-9]{2}")
+    }
+}
+
+
+
+class ShopController {
+
+    //def creditCardService
+
+    def index = {
+        redirect(action: "order")
+    }
+
+
+
+    def orderFlow = {
+
+
+        displayProducts {
+            on("next") { OrderDetailsCommand odc ->
+                if (odc.hasErrors() || odc.isOrderBlank()) {
+                    flow.orderDetails = odc
+                    return error()
+                }
+                [ orderDetails: odc, orderStartDate: new Date() ]
+            }.to("finish")
+            on("cancel").to("finish")
+        }
+
+
+
+        finish {
+            //redirect(controller:"homePage", action: "index")
+            redirect(uri:"/")
+        }
+
+
+    }
+}
---
+++ registroacegi/test/unit/com/grailsinaction/ShopControllerTests.groovy
@@ -0,0 +1,17 @@
+package com.grailsinaction
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(ShopController)
+class ShopControllerTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
